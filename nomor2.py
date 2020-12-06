3194863581ee8e2d115f418f861ec84629365c6c
lincolnhalim <75430250+lincolnhalim@users.noreply.github.com>
2020-12-06 15:18:09 +0700

    Add files via upload

---
+++ nomor2.py
@@ -0,0 +1,18 @@
+
+def counter(x):
+    hasil = list(map(list, zip(*x)))[::-1]
+    print("", hasil[0], '\n', hasil[1], '\n', hasil[2])
+
+A = [[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]
+
+B = [[9, 8, 7],
+[6, 5, 4],
+[3, 2, 1]]
+
+counter(A)
+print("="*50)
+counter(B)
+
+
